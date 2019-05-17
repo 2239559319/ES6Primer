0a43a8df4553e785c9b792433a62c812a12be727
2239559319 <2017141461249@stu.scu.edu.cn>
2019-05-17 21:52:13 +0800

    repeat方法

---
+++ 第4章/4.8repeat/4.8.js
@@ -0,0 +1,6 @@
+//repeat()
+// repeat()方法返回一个新字符串，表示将原字符串重复n次
+
+console.log('x'.repeat(3));     //'xxx'
+console.log('hello'.repeat(2));     //'hellohello'
+console.log('na'.repeat(0));        //''
